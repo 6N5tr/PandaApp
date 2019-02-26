ca10e68826daed86f18f46a5476b711c5e2717d7
David Mejia <franklin.mejia@epn.edu.ec>
2019-02-26 12:18:25 -0500

    Layout Ventas

--- app/src/main/java/com/example/pandaapp/VentasActivity.kt
+++ app/src/main/java/com/example/pandaapp/VentasActivity.kt
@@ -3,10 +3,10 @@ package com.example.pandaapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class InfoActivity : AppCompatActivity() {
+class VentasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info)
+        setContentView(R.layout.activity_ventas)
     }
 }
